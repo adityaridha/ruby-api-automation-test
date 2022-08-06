1957bf30056d60f5d1b9c8a6a92258774abaa5e8
Aditya Ridharrahman <adityaridharrahman@gmail.com>
2022-08-07 06:03:06 +0700

    add json path validator

--- features/reqresin.feature
+++ features/reqresin.feature
@@ -3,6 +3,7 @@ Feature: API Test using Reqresrin API
     Scenario: Send GET User request
         When user send GET User request
         Then user should get API response 200
+        And response should have "$.data.id" matching "2"
 
     Scenario: Send POST login request
         When user send GET User request
--- features/step_definitions/reqresrin_stepdefs.rb
+++ features/step_definitions/reqresrin_stepdefs.rb
@@ -1,4 +1,5 @@
 require_relative  '../../service/reqresin'
+require 'jsonpath'
 
 reqres = Reqresin.new
 
@@ -12,4 +13,10 @@ end
   
 Then('user should get API response {int}') do |res|
     expect(@response.code).to eq res
+end
+
+Then('response should have {string} matching {string}') do |json_path, value|
+    # result is an array
+    results = JsonPath.new(json_path).on(@response).to_a.map(&:to_s)
+    expect(results).to include value
 end
